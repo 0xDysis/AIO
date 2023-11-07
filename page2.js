060947d582281f0dd2dfb2077f2c452bfdbccc9d
0xDysis <99392905+0xDysis@users.noreply.github.com>
2023-11-08 00:02:31 +0100

    Add files via upload

--- page2.js
+++ page2.js
@@ -1,6 +1,7 @@
 const articles = [
     { title: "ΛSSIMILATION 7213", url: "1.html" },
     { title: "ΗEARTBEAT 7214", url: "2.html" },
+    { title: "PR0LIFERATE 7235", url: "3.html" },
     // Add more articles here
   ];
   
